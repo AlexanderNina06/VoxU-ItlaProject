0248302924e98f1b8d7c5a6f5cf417eff1ec165f
Nicol Cuello <nca271003@gmail.com>
2025-02-09 17:14:08 -0400

    Creacion del EmailService ;)

---
+++ VoxU-Backend.Persistence.Shared/Service/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoxU_Backend.Core.Application.DTOS.Email;
+using VoxU_Backend.Core.Domain.Settings;
+
+namespace VoxU_Backend.Persistence.Shared.Service
+{
+    public class EmailService
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendAsync(EmailRequest emailRequest)
+        {
+            //Almando el mensaje que es del tipo mimeMessage
+            MimeMessage Email = new();
+            Email.Sender = MailboxAddress.Parse($"{_mailSettings.DisplayName} <{_mailSettings.EmailFrom}>");
+            Email.To.Add(MailboxAddress.Parse(emailRequest.To));
+            Email.Subject = emailRequest.Subject;
+            BodyBuilder bodyBuilder = new();
+            bodyBuilder.HtmlBody = emailRequest.Body;
+            Email.Body = bodyBuilder.ToMessageBody();
+
+            try
+            {
+                using SmtpClient smtpClient = new();
+                smtpClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                await smtpClient.ConnectAsync(_mailSettings.SmtpHost, _mailSettings.SmtpPort, MailKit.Security.SecureSocketOptions.StartTls);
+                smtpClient.Authenticate(_mailSettings.SmtpUser, _mailSettings.SmtpPass);
+                await smtpClient.SendAsync(Email);
+                smtpClient.Disconnect(true);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+    }
+}
