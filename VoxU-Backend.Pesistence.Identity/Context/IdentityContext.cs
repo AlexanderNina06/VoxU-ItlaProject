ec959eae31247108d345e3d1f3ccaf8e392d1e1f
JeisonRd <jeisonrafael11@gmail.com>
2025-02-09 18:23:46 -0400

    Identity configuration

---
+++ VoxU-Backend.Pesistence.Identity/Context/IdentityContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using VoxU_Backend.Pesistence.Identity.Entities;
+
+namespace VoxU_Backend.Pesistence.Identity.Context
+{
+    public class IdentityContext : IdentityDbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            //Schema definition 
+            modelbuilder.HasDefaultSchema("IdentitySchema");
+
+
+            modelbuilder.Entity<ApplicationUser>().ToTable("Users");
+            modelbuilder.Entity<IdentityRole>().ToTable("IdentityRole");
+            modelbuilder.Entity<IdentityUserLogin<string>>().ToTable("IdentityUserLogin");
+            modelbuilder.Entity<IdentityUserRole<string>>().ToTable("IdentityUser");
+        }
+
+
+
+    }
+}
---
+++ VoxU-Backend.Pesistence.Identity/ServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoxU_Backend.Pesistence.Identity
+{
+    public static class ServiceRegistration
+    {
+    }
+}
