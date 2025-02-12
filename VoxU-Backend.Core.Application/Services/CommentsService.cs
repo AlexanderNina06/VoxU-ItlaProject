d637df721fee63bf99da4c6eca2e30fd27cc300f
JeisonRd <jeisonrafael11@gmail.com>
2025-02-12 19:52:41 -0400

    mapper basic

---
+++ VoxU-Backend.Core.Application/Services/CommentsService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoxU_Backend.Core.Application.DTOS.Comments;
+using VoxU_Backend.Core.Application.Interfaces.Repositories;
+using VoxU_Backend.Core.Domain.Entities;
+
+namespace VoxU_Backend.Core.Application.Services
+{
+    public class CommentsService : GenericService<GetCommentsResponse, SaveCommentsRequest, Comments>
+    {
+        private readonly ICommentsRepository _commentsRepository;
+        private readonly IMapper _mapper;
+       public CommentsService(ICommentsRepository commentsRepository, IMapper mapper) : base(mapper, commentsRepository)
+       {
+            _commentsRepository = commentsRepository;
+            _mapper = mapper;
+       }
+
+
+    }
+}
