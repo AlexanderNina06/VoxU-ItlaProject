fb01af2abdea889d77513db887091b0a8c425964
JeisonRd <jeisonrafael11@gmail.com>
2025-02-11 21:16:54 -0400

    Identity program configuration

--- VoxU-Backend/Program.cs
+++ VoxU-Backend/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using VoxU_Backend.Core.Persistence;
+using VoxU_Backend.Pesistence.Identity.Entities;
+using VoxU_Backend.Pesistence.Identity.Seeds;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,28 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+using (var scope = app.Services.CreateScope()) // Cada vez que hacemos injecciones de dependencia esto es lo que ocurre detras, estas se inicializan con el starup, en este caso no usamos startup, pero simplemente creamos una injeccion propia que se ejecuta antes del app.run
+{
+    var services = scope.ServiceProvider; // Recupero servicios 
+
+    try
+    { // Realizo las injecciones de dependencia 
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>(); // Con los servicios puedo recuperar esos objetos - clases a injectar 
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        // Ahora creamos los roles primero antes que los usuarios
+
+        await BasicUser.SeedAsync(userManager, roleManager);
+        await AdminUser.SeedAsync(userManager, roleManager);
+
+    }
+    catch (Exception ex)
+    {
+
+    }
+
+
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
