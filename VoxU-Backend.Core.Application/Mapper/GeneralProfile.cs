9900c77ddb3b002eb5f7b3cf43aec115efe5203a
Enrique Nina <enriquenina06@hotmail.com>
2025-02-12 20:30:51 -0400

    Add Mappings

--- VoxU-Backend.Core.Application/Mapper/GeneralProfile.cs
+++ VoxU-Backend.Core.Application/Mapper/GeneralProfile.cs
@@ -4,13 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoxU_Backend.Core.Application.DTOS.Publication;
+using VoxU_Backend.Core.Domain.Entities;
 
 namespace VoxU_Backend.Core.Application.Mapper
 {
     public class GeneralProfile : Profile
     {
+        public GeneralProfile()
+        {
+            CreateMap<Publications, SavePublicationRequest>()
+                .ReverseMap()
+              .ForMember(dest => dest.Comments, opt => opt.Ignore());
 
-
-
+            CreateMap<Publications, GetPublicationResponse>()
+             .ForMember(dest => dest.userPicture, opt => opt.Ignore())
+             .ForMember(dest => dest.userName, opt => opt.Ignore())
+             .ForMember(dest => dest.CommentsCount, opt => opt.Ignore())
+             .ReverseMap();
+        }
     }
 }
