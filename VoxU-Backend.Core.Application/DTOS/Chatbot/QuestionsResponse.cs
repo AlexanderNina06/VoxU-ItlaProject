ab3e703efe57fe0ab0760cb671b0199266000e12
JEISON INFANTE <jeison_infante@bhd.com.do>
2025-03-18 21:36:49 -0400

    chatbot implementation

---
+++ VoxU-Backend.Core.Application/DTOS/Chatbot/QuestionsResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoxU_Backend.Core.Application.DTOS.Chatbot
+{
+    public class QuestionsResponse
+    {
+        public List<string> Questions { get; set; } = (["Como estas ?",
+            "",
+            "",
+            ]);
+
+
+    }
+}
---
+++ VoxU-Backend.Persistence.Shared/Service/ChatbotService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Options;
+using Org.BouncyCastle.Asn1.Cmp;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using VoxU_Backend.Core.Application.DTOS.Chatbot;
+using VoxU_Backend.Core.Application.Interfaces.Services;
+using VoxU_Backend.Persistence.Shared.Options;
+
+
+namespace VoxU_Backend.Persistence.Shared.Service
+{
+    public class ChatbotService : IChatbotService
+    {
+        private readonly HttpClient _httpClientFactory;
+        private DeepSeekSettings _deepSeekSettings;
+     
+
+        public ChatbotService(HttpClient httpClientFactory, IOptions<DeepSeekSettings> deepSeekSettings)
+        {
+            _httpClientFactory = httpClientFactory;
+            _deepSeekSettings = deepSeekSettings.Value;
+        }
+
+
+        public async Task<ChatbotResponse> getChatbotReponseAsync(string prompt)
+        {
+
+
+            var jsonPayload = JsonSerializer.Serialize(prompt);
+            var httpContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+            _httpClientFactory.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _deepSeekSettings.API_KEY);
+
+            try
+            {
+                var response = await _httpClientFactory.PostAsync(_deepSeekSettings.URL, httpContent);
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var responseJson = JsonSerializer.Deserialize<JsonElement>(responseContent);
+
+                return new ChatbotResponse
+                {
+                    Reponse = responseJson.GetProperty("choices")[0].GetProperty("message").GetProperty("content").GetString()
+                };
+            }
+            catch (HttpRequestException e)
+            {
+                throw new Exception("Error en la solicitud: " + e.Message);
+            }
+        }
+
+    }
+
+
+    }
+
